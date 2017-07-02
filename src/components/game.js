3b5dd67ea0d3f94be581cf061c543715094e15a0
kamblepradnya <pradnyakamble07@gmail.com>
2017-07-03 00:23:59 +0530

    Add files via upload

---
+++ src/components/game.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Board from './board';
+
+
+export default class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+      </div>
+    );
+  }
+}  
+
